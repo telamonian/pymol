fa6bf881089d78dfbdd1d770ef01522c01f88ae9
telamonian <max.klein@jhu.edu>
2017-08-03 19:08:58 -0400

    OSX GUI fix with no new dependencies
    - A better, less breaking fix for the external gui on OSX when using tkinter built against aqua.
    
    + Alters pymol execution behavior only if all of the following are true:
    
    * external gui requested
    * pymol is launched on a mac (ie `sys.platform=='darwin'`)
    * Tkinter is using aqua as its windowing system
    
    - Unlike first fix version, avoids a dependency on `mttkinter` python package.
    
    + At the cost of slightly delaying plugin initialization.
    
    * Initial testing didn't turn up any plugin-related problems, but maybe this will cause issues with certain plugins? Hard to test

--- modules/pmg_tk/__init__.py
+++ modules/pmg_tk/__init__.py
@@ -23,18 +23,47 @@ from .PMGApp import *
 import sys, os, threading
 import traceback
 
-def run(pymol_instance,poll=0,skin=None):
+def run(pymol_instance,poll=0,skin=None,root=None):
     try:
         if not hasattr(sys,"argv"):
             sys.argv=["pymol"]
-        PMGApp(pymol_instance,skin).run(poll)
+        PMGApp(pymol_instance,skin,root).run(poll)
     except:
         traceback.print_exc()
-        
-def __init__(pymol_instance,poll=0,skin=None):
-    t = threading.Thread(target=run,args=(pymol_instance,poll,skin))
-    t.setDaemon(1)
-    t.start()
+
+def __init__(pymol_instance,poll=0,skin=None,root=None):
+    if root is not None:
+        run(pymol_instance=pymol_instance, poll=poll, skin=skin, root=root)
+    else:
+        t = threading.Thread(target=run,args=(pymol_instance,poll,skin))
+        t.setDaemon(1)
+        t.start()
+
+# from .PMGApp import *
+# import sys, os, threading
+# import traceback
+#
+# def run(pymol_instance,poll=0,skin=None):
+#     try:
+#         initApp(pymol_instance, skin=skin).run(poll)
+#     except:
+#         traceback.print_exc()
+#
+# def initApp(pymol_instance,skin=None,root=None):
+#     try:
+#         if not hasattr(sys,"argv"):
+#             sys.argv=["pymol"]
+#         return PMGApp(pymol_instance,skin,root)
+#     except:
+#         traceback.print_exc()
+#
+# def __init__(pymol_instance,poll=0,skin=None):
+#     import ipdb; ipdb.set_trace()
+#     run(pymol_instance=pymol_instance, poll=poll, skin=skin)
+#
+#     # t = threading.Thread(target=run,args=(pymol_instance,poll,skin))
+#     # t.setDaemon(1)
+#     # t.start()
 
 
 
