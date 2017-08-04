6f90fac274fc0d463aa6d26ff19a7731149da5dc
telamonian <max.klein@jhu.edu>
2017-08-03 23:08:21 -0400

    Bugfix/cleanup, but program still isn't exiting quite correctly

--- modules/pmg_tk/__init__.py
+++ modules/pmg_tk/__init__.py
@@ -31,41 +31,10 @@ def run(pymol_instance,poll=0,skin=None,root=None):
     except:
         traceback.print_exc()
 
-def __init__(pymol_instance,poll=0,skin=None,root=None):
-    if root is not None:
-        run(pymol_instance=pymol_instance, poll=poll, skin=skin, root=root)
-    else:
-        t = threading.Thread(target=run,args=(pymol_instance,poll,skin))
+def __init__(pymol_instance,poll=0,skin=None,root=None,usethread=True):
+    if usethread:
+        t = threading.Thread(target=run,args=(pymol_instance,poll,skin,root))
         t.setDaemon(1)
         t.start()
-
-# from .PMGApp import *
-# import sys, os, threading
-# import traceback
-#
-# def run(pymol_instance,poll=0,skin=None):
-#     try:
-#         initApp(pymol_instance, skin=skin).run(poll)
-#     except:
-#         traceback.print_exc()
-#
-# def initApp(pymol_instance,skin=None,root=None):
-#     try:
-#         if not hasattr(sys,"argv"):
-#             sys.argv=["pymol"]
-#         return PMGApp(pymol_instance,skin,root)
-#     except:
-#         traceback.print_exc()
-#
-# def __init__(pymol_instance,poll=0,skin=None):
-#     import ipdb; ipdb.set_trace()
-#     run(pymol_instance=pymol_instance, poll=poll, skin=skin)
-#
-#     # t = threading.Thread(target=run,args=(pymol_instance,poll,skin))
-#     # t.setDaemon(1)
-#     # t.start()
-
-
-
-
-
+    else:
+        run(pymol_instance=pymol_instance, poll=poll, skin=skin, root=root)
